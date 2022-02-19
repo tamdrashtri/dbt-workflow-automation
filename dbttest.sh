c4ffae376cb0bd97b8a2ed699ba6e51451a9e842
Tam Nguyen <tam.nguyen@bench.co>
2022-02-18 17:17:37 -0800

    added step 2 and outline a few todos

--- dbttest.sh
+++ dbttest.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+# what steps are you in
+# dbt deploy helper
 echo "Let's first synchronize your local files to the latest version"
 echo "Start by navigating to your local dbt directory."
 
+# could you confirm if this is your dbt directory? if not, please paste your directory for dbt here.
 cd ~/github/bench-analytics/dbt
 
 git checkout master && git pull
+
+make clean-sandbox && make artifacts
+
+# Step 2: Now let's create a new branch
+
+echo `Please tell us the branch name you want to create. A good example is "BI-1234-add-widget-state-to-whizbang-model"`
+read BRANCH_NAME
+
+git checkout -b $BRANCH_NAME
+
+echo "Are you ready to commit your changes?"
+git add .
